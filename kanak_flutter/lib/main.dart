b2d2937e1ec569c43b870f8411740300a55b857e
Abhimanyu M A <abhi@manyu.in>
2021-11-07 17:54:02 +0000

    Add a Map and an answer widget
    This converts the answer into a separate widget, and adds a map for the question answer section.
    
    This introduces us to voidcallback method and maps in dart.
    
    This is still part of the tutorial code of dart and flutter.

--- kanak_flutter/lib/main.dart
+++ kanak_flutter/lib/main.dart
@@ -1,10 +1,7 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
 
 import './question.dart';
+import './answer.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,20 +12,47 @@ class MyApp extends StatefulWidget {
   }
 }
 
-
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
-  var _questions = [
-    "What do you want to do with this app?",
-    "Do you know how much you are worth?",
-    "Where do you live in terms of taxation?",
+  final _qanda = [
+    {
+      "questionText": "What do you want to do with this app?",
+      "answers": [
+        "Budget my expenses",
+        "Track my investments",
+        "Plan my taxes",
+        "Pay off Debt",
+        "Other",
+      ]
+    },
+    {
+      "questionText": "Do you know how much you are worth",
+      "answers": [
+        "Yes, but I owe more than I have",
+        "Yes, but it is under 1 Million",
+        "Yes, and I have more than 1 Million",
+        "No",
+        "It is complicated",
+        "Other",
+      ]
+    },
+    {
+      "questionText": "Where do you live in terms of taxation",
+      "answers": [
+        "USA",
+        "UK",
+        "India",
+        "I am not a tax resident anywhere",
+        "Other",
+      ]
+    },
   ];
 
   void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
-      _questionIndex = _questionIndex % _questions.length;
-    });    
+      _questionIndex = _questionIndex % _qanda.length;
+    });
   }
 
   @override
@@ -40,10 +64,10 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Welcome to Kanak'),
         ),
         body: Column(children: [
-          Question(_questions[_questionIndex]),
-          RaisedButton(child: Text('Answer 1'), onPressed: _answerQuestion),
-          RaisedButton(child: Text('Answer 2'), onPressed: _answerQuestion),
-          RaisedButton(child: Text('Answer 3'), onPressed: _answerQuestion),
+          Question(_qanda[_questionIndex]["questionText"] as String),
+          ...(_qanda[_questionIndex]["answers"] as List<String>).map((answer) {
+            return Answer(answer, _answerQuestion);
+          }).toList(),
         ]),
       ),
     );
